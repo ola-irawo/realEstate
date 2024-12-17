9cf3bfba56e63f292b186cfae58bc91014a4d22c
Irawo <timothyaremu63@gmail.com>
2024-12-17 17:40:18 +0100

    updated the files

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['firebasestorage.googleapis.com', 'img.freepik.com'], // Add the Firebase storage domain
+    domains: ['firebasestorage.googleapis.com', 'img.freepik.com', "example.com"], // Add the Firebase storage domain
   },
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint during build
