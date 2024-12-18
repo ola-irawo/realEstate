e75353d6af1864e6c96a50d82987a850c6d3c435
Irawo <timothyaremu63@gmail.com>
2024-12-18 21:03:06 +0100

    updated

--- src/features/pages/propertyListing/layout/PropertyListingLayout.tsx
+++ src/features/pages/propertyListing/layout/PropertyListingLayout.tsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 import PropertyListingHero from '../sections/propertyListingHero/PropertyListingHero'
 import PropertyListing from '../sections/propertyListing/PropertyListing'
 import { useFilteredPropertyQuery } from '@/redux/features/propertiesApi/propertiesApi'
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
 import Loader from '@/component/loader/Loader'
 
 const PropertyListingLayout = () => {
-    const searchParams = useSearchParams()
+    // const searchParams = useSearchParams()
     // const propertyType = searchParams.get("propertyType")
     const propertyType = "sale"
 
