18265d2e3be70d6db898882a2ec0d45a0da1ea3b
Irawo <timothyaremu63@gmail.com>
2024-12-18 20:46:37 +0100

    added suspense to handle async behaviour

--- src/features/pages/propertyListing/layout/PropertyListingLayout.tsx
+++ src/features/pages/propertyListing/layout/PropertyListingLayout.tsx
@@ -1,48 +1,52 @@
-"use client"
-import React, { useState } from 'react'
-import PropertyListingHero from '../sections/propertyListingHero/PropertyListingHero'
-import PropertyListing from '../sections/propertyListing/PropertyListing'
-import { useFilteredPropertyQuery } from '@/redux/features/propertiesApi/propertiesApi'
-import Loader from '@/component/loader/Loader'
+"use client";
+
+import React, { useState, Suspense } from 'react';
+import PropertyListingHero from '../sections/propertyListingHero/PropertyListingHero';
+import PropertyListing from '../sections/propertyListing/PropertyListing';
+import { useFilteredPropertyQuery } from '@/redux/features/propertiesApi/propertiesApi';
+import { useSearchParams } from 'next/navigation';
+import Loader from '@/component/loader/Loader';
 
 const PropertyListingLayout = () => {
-    // const searchParams = useSearchParams()
-    // const propertyType = searchParams.get("propertyType")
-    const propertyType = "sale"
+  const searchParams = useSearchParams();
+  const propertyType = searchParams.get("propertyType");
 
-    const [propertyFilters, setPropertyFilters] = useState({
-        type: propertyType ? propertyType : "sale",
-        price: "",
-        location: "",
-        bathrooms: 0,
-        bedrooms: 0,
-    });
+  const [propertyFilters, setPropertyFilters] = useState({
+    type: propertyType ? propertyType : "sale",
+    price: "",
+    location: "",
+    bathrooms: 0,
+    bedrooms: 0,
+  });
 
-    const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        setPropertyFilters((prevFilters) => ({
-            ...prevFilters,
-            [name]: value,
-        }));
-    };
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setPropertyFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+  };
 
-    const {
-      data: properties = [],
-      isLoading,
-    } = useFilteredPropertyQuery({propertyFilters, propertyType: propertyFilters.type})
+  const {
+    data: properties = [],
+    isLoading,
+  } = useFilteredPropertyQuery({
+    propertyFilters,
+    propertyType: propertyFilters.type,
+  });
 
-    if(isLoading){
-      return <Loader />
-    }
+  if (isLoading) {
+    return <Loader />;
+  }
 
-    console.log(properties)
-    
+  console.log(properties);
+
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <PropertyListingHero propertyFilters={propertyFilters} handleFilterChange={handleFilterChange} />
       <PropertyListing properties={properties} type={propertyFilters.type} />
-    </>
-  )
-}
+    </Suspense>
+  );
+};
 
-export default PropertyListingLayout
+export default PropertyListingLayout;
