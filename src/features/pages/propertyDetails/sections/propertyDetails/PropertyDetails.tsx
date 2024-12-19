0d246675296bfca6d6b4c56f36d7ddee0917bcb1
Irawo <timothyaremu63@gmail.com>
2024-12-19 15:23:55 +0100

    added options to contact actions

--- src/features/pages/propertyDetails/sections/propertyDetails/PropertyDetails.tsx
+++ src/features/pages/propertyDetails/sections/propertyDetails/PropertyDetails.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./property-details.module.css"
+import Link from 'next/link';
+import BookInspection from '@/component/modal/bookInspection/BookInspection';
 
 interface PropertyProps {
     images: string[];
@@ -15,6 +17,7 @@ interface PropertyProps {
     bathrooms: number;
     description: string;
     features: string[];
+    id?: string
   }
   
   interface Props {
@@ -22,35 +25,59 @@ interface PropertyProps {
   }
 
 const PropertyDetails = ({property}: Props) => {
+  const [openBookInspection, setOpenBookInspection] = useState<boolean>((false))
+  
   return (
     <section className={styles.detailsSection}>
-        <article className={styles.detailsContainer}>
-            <h2 className={styles.propertyName}>{property.title}</h2>
-            <p className={styles.propertyType}>For {property.type}</p>
-            
-            <div className={styles.location}>
-                <p>{`${property.location.city}, ${property.location.state}, ${property.location.country}`}</p>
-                <p>{`Price: $${property.price.toLocaleString()}`}</p>
-                <p>{`Bedrooms: ${property.bedrooms}`}</p>
-                <p>{`Bedrooms: ${property.bathrooms}`}</p>
-            </div>
-            
-            
-            <div className={styles.description}>
-                <h2>Description</h2>
-                <p>{property.description}</p>
-            </div>
-            
-            <div className={styles.features}>
-                <h3>Features</h3>
-                <ul>
-                    {property.features.map((feature: string, index: number) => (
-                    <li key={index}>{feature}</li>
-                    ))}
-                </ul>
-            </div>
+      {openBookInspection && 
+      <BookInspection 
+        propertyLink={`${window.location}/${property.id}/${property.type}`}
+        setOpenBookInspection={setOpenBookInspection}
+        title={property.title}
+        type={property.type}
+      />}
 
-        </article>
+      <article className={styles.detailsContainer}>
+        <div className={styles.actionsContainer}>
+          <a 
+            href={"tel:+2348156959983"} 
+            className={styles.contactAgentButton}
+          >
+            Contact Agent
+          </a>
+          <button 
+            className={styles.bookInspectionButton}
+            onClick={() => setOpenBookInspection(true)}
+          >
+            Book Inspection
+          </button>
+        </div>
+
+        <h2 className={styles.propertyName}>{property.title}</h2>
+        <p className={styles.propertyType}>For {property.type}</p>
+        
+        <div className={styles.location}>
+          <p>{`${property.location.city}, ${property.location.state}, ${property.location.country}`}</p>
+          <p>{`Price: $${property.price.toLocaleString()}`}</p>
+          <p>{`Bedrooms: ${property.bedrooms}`}</p>
+          <p>{`Bedrooms: ${property.bathrooms}`}</p>
+        </div>
+        
+        
+        <div className={styles.description}>
+          <h2>Description</h2>
+          <p>{property.description}</p>
+        </div>
+        
+        <div className={styles.features}>
+          <h3>Features</h3>
+          <ul>
+            {property.features.map((feature: string, index: number) => (
+            <li key={index}>{feature}</li>
+            ))}
+          </ul>
+        </div>
+      </article>
     </section>
   )
 }
